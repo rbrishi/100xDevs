97ae4de9061fd093d2f59161a98a22021ff9f5a8
Rishabh Bajpai <98521911+rbrishi@users.noreply.github.com>
2024-08-01 22:49:00 +0000

    #

--- Week-8/8.1 Tailwind CSS/src/App.jsx
+++ Week-8/8.1 Tailwind CSS/src/App.jsx
@@ -6,13 +6,21 @@ function App() {
 
   return (
     <>
+      {/* raw css */}
       <div style={{ display: "flex", justifyContent: "center" }}>
         <div style={{ backgroundColor: "red" }}>hi</div>
         <div style={{ backgroundColor: "green" }}>hi</div>
         <div style={{ backgroundColor: "blue" }}>hi</div>
       </div>
-
+      {/* Tailwind CSS */}
       <div className="flex justify-between">
+        <div className="bg-red-500">hello</div>
+        <div className="bg-green-500">hello</div>
+        <div className="bg-blue-500">hello</div>
+        <div className="bg-yellow-500">hello</div>
+      </div>
+      <br></br>
+      <div className="grid grid-cols-4">
         <div className="bg-red-500">hello</div>
         <div className="bg-green-500">hello</div>
         <div className="bg-blue-500">hello</div>
